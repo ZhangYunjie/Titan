304d815bd70386005b22c193018c33be463d746c
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-06-11 13:51:26 +0900

    base scene実装

--- Classes/Base/TitanScene.h
+++ Classes/Base/TitanScene.h
@@ -13,13 +13,13 @@
 
 #include "TitanBase.h"
 
-#define SCENE_FUN(__TYPE__) \
+#define SCENE_FUNC(__TYPE__) \
 static TTSceneBaseScene* scene() \
 { \
-    TTSceneBaseScene* _scene = TTSceneBaseScene::create(); \
+    auto _scene = TTSceneBaseScene::create(); \
     auto _node = __TYPE__::create(); \
     _scene->addChild(_node); \
-    _scene->setTitainScene(dynamic_cast<TTSceneBaseScene*>(_node)); \
+    _scene->setTTScene(dynamic_cast<TTSceneBaseLayer*>(_node)); \
     return _scene; \
 }
 
@@ -42,17 +42,95 @@ public:
 
 class TTSceneBaseScene : public cocos2d::Scene
 {
-    TTSceneBaseScene* scene;
+    TTSceneBaseLayer* scene;
 
 protected:
     TTSceneBaseScene():scene(NULL){}
 
 public:
     void setTTScene(TTSceneBaseLayer* _scene){scene=_scene;}
-    TTSceneBaseScene* getTTScene(){return scene;}
+    TTSceneBaseLayer* getTTScene(){return scene;}
     
     CREATE_FUNC(TTSceneBaseScene);
-}
+};
+
+template<class T_c> class TTScene : public TTSceneBaseLayer
+{
+    bool interaction;
+    
+protected:
+    static T_c* _singleton;
+    
+public:
+    TTScene():interaction(false), inheritance(NULL){}
+    virtual ~TTScene(){}
+    
+    virtual bool init()
+    {
+        if(!cocos2d::LayerColor::init())
+        {
+            return false;
+        }
+        
+        _singleton = dynamic_cast<T_c*>(this);
+        _singleton->setTouchEnabled(interaction);
+        
+        initScene();
+        
+        setInterrupt(true);
+        return _singleton? true : false;
+    }
+    
+    void doInheritance()
+    {
+        if(inheritance)
+        {
+            inheritance();
+        }
+    }
+    
+    virtual void initScene(){}
+    virtual void termScene(){}
+    
+    template<class T_cPush> static void pushScene()
+    {
+        _singleton->setInterrupt(false);
+        _singleton->termScene();
+        cocos2d::Director::getInstance()->pushScene(T_cPush::scene());
+    }
+    
+    static void popScene()
+    {
+        _singleton->setInterrupt(false);
+        _singleton->termScene();
+        cocos2d::Director::getInstance()->popScene();
+        
+        // TODO: next scene's interaction
+        
+        _singleton->TTScene::doInheritance();
+    }
+    
+    template<class T_cReplace> static void replaceScene()
+    {
+        _singleton->setInterrupt(false);
+        _singleton->termScene();
+        cocos2d::Director::getInstance()->replaceScene(T_cReplace::scene());
+    }
+    
+    template<class T_cScene> static void topScene()
+    {
+        _singleton->setInterrupt(false);
+        _singleton->termScene();
+        
+        // TOOD: topScene実装
+        // cocos2d::Director::getInstance()->topScene(T_cScene::scene());
+    }
+    
+    SCENE_FUNC(T_c);
+    CREATE_FUNC(T_c);
+
+    void (*inheritance)();
+};
 
 NS_TT_END
 
