4d08aa82c39ba5b4c493588eebadb03325b86b71
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-06-11 13:52:10 +0900

    spaceを消す

--- Classes/Base/TitanScene.h
+++ Classes/Base/TitanScene.h
@@ -28,11 +28,11 @@ NS_TT_BEGIN
 class TTSceneBaseLayer : public cocos2d::LayerColor
 {
     bool interruption;
-    
+
 protected:
     TTSceneBaseLayer():interruption(false){}
     void setInterrupt(bool _interruption){interruption = _interruption;}
-    
+
 public:
     bool getInterrupt(){return interruption;}
 
@@ -50,37 +50,37 @@ protected:
 public:
     void setTTScene(TTSceneBaseLayer* _scene){scene=_scene;}
     TTSceneBaseLayer* getTTScene(){return scene;}
-    
+
     CREATE_FUNC(TTSceneBaseScene);
 };
 
 template<class T_c> class TTScene : public TTSceneBaseLayer
 {
     bool interaction;
-    
+
 protected:
     static T_c* _singleton;
-    
+
 public:
     TTScene():interaction(false), inheritance(NULL){}
     virtual ~TTScene(){}
-    
+
     virtual bool init()
     {
         if(!cocos2d::LayerColor::init())
         {
             return false;
         }
-        
+
         _singleton = dynamic_cast<T_c*>(this);
         _singleton->setTouchEnabled(interaction);
-        
+
         initScene();
-        
+
         setInterrupt(true);
         return _singleton? true : false;
     }
-    
+
     void doInheritance()
     {
         if(inheritance)
@@ -88,44 +88,44 @@ public:
             inheritance();
         }
     }
-    
+
     virtual void initScene(){}
     virtual void termScene(){}
-    
+
     template<class T_cPush> static void pushScene()
     {
         _singleton->setInterrupt(false);
         _singleton->termScene();
         cocos2d::Director::getInstance()->pushScene(T_cPush::scene());
     }
-    
+
     static void popScene()
     {
         _singleton->setInterrupt(false);
         _singleton->termScene();
         cocos2d::Director::getInstance()->popScene();
-        
+
         // TODO: next scene's interaction
-        
+
         _singleton->TTScene::doInheritance();
     }
-    
+
     template<class T_cReplace> static void replaceScene()
     {
         _singleton->setInterrupt(false);
         _singleton->termScene();
         cocos2d::Director::getInstance()->replaceScene(T_cReplace::scene());
     }
-    
+
     template<class T_cScene> static void topScene()
     {
         _singleton->setInterrupt(false);
         _singleton->termScene();
-        
+
         // TOOD: topScene実装
         // cocos2d::Director::getInstance()->topScene(T_cScene::scene());
     }
-    
+
     SCENE_FUNC(T_c);
     CREATE_FUNC(T_c);
 
