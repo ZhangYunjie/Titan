8d5e9bcfccd9713984d95a3c70789b87510d42bd
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-06-17 18:50:42 +0900

    JSON Classを作る

---
+++ Classes/Common/JSON.cpp
@@ -0,0 +1,9 @@
+//
+//  JSON.cpp
+//  Titan
+//
+//  Created by zhang yunjie on 2014/06/17.
+//
+//
+
+#include "JSON.h"
---
+++ Classes/Common/JSON.h
@@ -0,0 +1,19 @@
+//
+//  JSON.h
+//  Titan
+//
+//  Created by zhang yunjie on 2014/06/17.
+//
+//
+
+#ifndef __Titan__JSON__
+#define __Titan__JSON__
+
+#include "picojson.h"
+
+class JSON
+{
+
+};
+
+#endif /* defined(__Titan__JSON__) */
