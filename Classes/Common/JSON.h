96a4e83dcba292dd05ca438878e3aa783f76de1a
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-06-17 19:42:06 +0900

    ヘーダ

--- Classes/Common/JSON.h
+++ Classes/Common/JSON.h
@@ -13,7 +13,30 @@
 
 class JSON
 {
+public:
+    typedef picojson::value  Value;
+    typedef picojson::object Object;
+    typedef picojson::array  Array;
+    typedef std::vector<const char*> Keys;
 
+    static void parse(Value &jvalue, const char *json);
+    static bool parseFile(Value &jvalue, const char *file);
+
+    static bool existKey(const Value &jvalue, const char *key);
+    static void getKeys(const Value &jvalue, Keys &keys);
+
+    static const Object*    getObject   (const Value &jvalue);
+    static const Value*     getValue    (const Value &jvalue, const char* key);
+    static const Array*     getArray    (const Value &jvalue, const char* key);
+
+    static std::string      getString   (const Value &jvalue, const char* key = NULL);
+    static double           getNumber   (const Value &jvalue, const char* key = NULL);
+    static bool             getBool     (const Value &jvalue, const char* key = NULL);
+
+    static void setObject(Object& base, const char* key, Array data);
+    static void setObject(Object& base, const char* key, Object data);
+    static void setObject(Object& base, const char* key, double data);
+    static void setObject
 };
 
 #endif /* defined(__Titan__JSON__) */
