c4ff9e1ff5e7bb3657b5f4b05e631c6a8c618496
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-06-24 19:46:24 +0900

    tmp

--- Classes/Scene/BattleScene/BattleWorld.h
+++ Classes/Scene/BattleScene/BattleWorld.h
@@ -33,9 +33,25 @@ public:
 
     b2Body* createB2Body(void* pUserData, b2BodyType bodyType, cocos2d::Point pos, const b2Shape& shape, float density, b2World* pWorld);
     b2Body* createB2BodyWithSprite(cocos2d::Sprite* sprite, b2BodyType bodyType, const b2Shape& shape, float density, b2World* pWorld);
+    
+    void marchingSquares(cocos2d::Image* image, std::vector<cocos2d::Vec2>& pointVector);
+    void RDP(std::vector<cocos2d::Vec2> &v, float epsilon, std::vector<cocos2d::Vec2> &resultVector);
+    
+    struct Pixel{
+        cocos2d::Color4B color;
+        cocos2d::Vec2    position;
+    };
+    
+    Pixel getPixel(cocos2d::Image* image, int x, int y);
 
 private:
     static BattleWorld* _singleton;
+    cocos2d::Image* _image;
+    int tolerance;
+
+    float findPerpendicularDisatance(cocos2d::Vec2 point, cocos2d::Vec2 p1, cocos2d::Vec2 p);
+    cocos2d::Vec2* getStartingPixel();
+    int getSquareValue(float px, float py);
 };
 
 #endif /* defined(__Titan__BattleWorld__) */
